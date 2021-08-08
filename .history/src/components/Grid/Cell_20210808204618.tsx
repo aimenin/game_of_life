90fd874bed3db996e50550dc06302a5008e1954f
aimenin <artem.igorevich8@rambler.ru>
2021-08-08 20:47:43 +0300

    added chromatic

---
+++ .history/src/components/Grid/Cell_20210808204618.tsx
@@ -0,0 +1,31 @@
+import React, { FC, useState } from 'react';
+import styled from '@emotion/styled';
+import { Coords, CellType } from '@/helpers/Field';
+
+export interface CellProps {
+  coords: Coords;
+  /**
+   * Cell coordinates
+   */
+}
+
+export const CellStyles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  background-color: red;
+  border: 1px solid black;
+  cursor: pointer;
+`;
+
+export const Cell: FC<CellProps> = ({ coords }) => {
+  const [click, setClick] = useState(false);
+
+  return (
+    <CellStyles onClick={() => setClick(!click)} role="cell">
+      {click && `${coords[0]}:${coords[1]}`}
+    </CellStyles>
+  );
+};
